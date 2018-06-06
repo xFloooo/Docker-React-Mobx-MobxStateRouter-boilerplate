31c9693200f4415410eb36fbfdb64fd2f0b19f54
xfloooo <xfloooo1@gmail.com>
2018-06-07 02:14:13 +0300

    add prod mode

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,30 @@ import * as express from "express";
 import * as bodyParser from "body-parser";
 import * as compression from "compression";
 import { AppConf } from "./conf";
-const webpack = require("webpack");
-const middleware = require("webpack-dev-middleware");
-const webpackConfig = require("../webpack.config.js");
-const compiler = webpack(webpackConfig);
-const instance = middleware(compiler, {
-  hot: true,
-  stats: {
-    colors: true
-  }
-});
+
+const isProduction = AppConf.get("NODE_ENV") == "production";
+
 const app = express();
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(instance);
-app.use(require("webpack-hot-middleware")(compiler));
+
+if (!isProduction) {
+  const webpack = require("webpack");
+  const middleware = require("webpack-dev-middleware");
+  const webpackConfig = require("../webpack.config.js");
+  const compiler = webpack(webpackConfig);
+  const instance = middleware(compiler, {
+    hot: true,
+    stats: {
+      colors: true
+    }
+  });
+  app.use(instance);
+  app.use(require("webpack-hot-middleware")(compiler));
+} else {
+  app.use(express.static("dist/frontend"));
+}
 
 const PORT = AppConf.get("APP_PORT") || 8009;
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
-console.log("--------------", AppConf.get("APP_PORT"));
